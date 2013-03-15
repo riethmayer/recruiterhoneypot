0cbfe51bec1d50915c9edb462897c82b58ba7fa7
Jan Riethmayer <jan@riethmayer.de>
2013-03-15 15:17:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recruiterhoneypot::Application.config.secret_token = '7e250bc9c94a9c0869e8eab298d43c98ff99cc76ef202c8396554f3fade3f3da24422b93e2e926bf15e79e6c4445cd24442ccd211814b77d8e42be5ba76742bd'
